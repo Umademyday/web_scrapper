894ae94b2f5c43aa50857054dfcda6ff5d277108
Misha <umademyday@gmail.com>
2023-12-22 17:43:16 +0100

    Version 0.04
    Does the core part + OOP + config file + filtering + aggs
    
    - No logginng
    - No testing

--- README.md
+++ README.md
@@ -2,4 +2,18 @@
 
 Assumptions I've made:
 - While counting words in the title, punctuation should be ignored, so "note-taking app" is 3 words not 2
-- In the meantime web links counts as a single word, so (da.vidbuchanan.co.uk) is 1 word, not 4
+- In a meantime web links counts as a single word, so (da.vidbuchanan.co.uk) is 1 word, not 4
+- News output type as well as filtering mode could be selected in file arguments
+- Filtering thresholds and news num to parse are in config.ini file
+
+Usage examples:
+python3 main.py
+This will run the script without any filtering or output file specified. Output will be made to stdinv.
+
+python3 main.py --filtering_mode 1
+This will run the script with filtering mode 1. (Filter all previous entries with more than five words in the title 
+ordered by the number of comments first.)
+
+python main.py --filtering_mode 2 --outputfile_name output.txt
+This will run the script with filtering mode 2 (Filter all previous entries with less than or equal to five words in 
+the title ordered by points.) and write the output to a file named output.txt.
--- main.py
+++ main.py
@@ -1,12 +1,24 @@
 import requests
-from typing import List
 from bs4 import BeautifulSoup
 import configparser
+import argparse
 
-from lib_classes.data_proc import NewsItem
 from lib_classes.data_proc import NewsFactory
 
+parser = argparse.ArgumentParser(description='Filter and output news items.')
 
+# Adding arguments
+parser.add_argument('--filtering_mode', type=int, choices=[1, 2], default=0,
+                    help='Filtering mode: 1 or 2 (no filtering if not specified)')
+parser.add_argument('--outputfile_name', type=str, default='',
+                    help='Output file name (if specified, output to the file)')
+
+# Parse and access the arguments
+args = parser.parse_args()
+filtering_mode = args.filtering_mode
+outputfile_name = args.outputfile_name
+
+# Parse configfile
 config = configparser.ConfigParser()
 config.read('config.ini')
 
@@ -14,7 +26,6 @@ URL = config['SETTINGS']['URL']
 NEWS_NUM = min(config.getint('SETTINGS', 'NEWS_NUM'), 30)
 WORDS_NUM_FILTER1 = config.getint('SETTINGS', 'WORDS_NUM_FILTER1')
 WORDS_NUM_FILTER2 = config.getint('SETTINGS', 'WORDS_NUM_FILTER1')
-filtering_type = 2
 
 page = requests.get(URL)
 soup = BeautifulSoup(page.content, "html.parser")
@@ -22,23 +33,25 @@ soup = BeautifulSoup(page.content, "html.parser")
 title_elements = soup.find_all("tr", class_="athing")
 subtext_elements = soup.find_all("td", class_="subtext")
 
-news_items: List[NewsItem] = []
-
-
 if __name__ == '__main__':
+    # Create news using Factory
+    news_items = [NewsFactory.create_from_soup(title_elements[i], subtext_elements[i]) for i in range(NEWS_NUM)]
 
-    for i in range(NEWS_NUM):
-        news_item = NewsFactory.create_from_soup(title_elements[i], subtext_elements[i])
-        news_items.append(news_item)
+    # Now filter according to args
+    if filtering_mode == 1:
+        print('Filtering mode 1 enabled')
+        news_items = sorted([x for x in news_items if x.words_count() > WORDS_NUM_FILTER1], key=lambda x: x.comments)
 
-    if filtering_type == 1:
-        news_items = [x for x in news_items if x.words_count() > WORDS_NUM_FILTER1]
-        news_items = sorted(news_items, key=lambda x: x.comments)
+    elif filtering_mode == 2:
+        print('Filtering mode 2 enabled')
+        news_items = sorted([x for x in news_items if x.words_count() <= WORDS_NUM_FILTER2], key=lambda x: x.score)
 
-    elif filtering_type == 2:
-        news_items = [x for x in news_items if x.words_count() <= WORDS_NUM_FILTER2]
-        news_items = sorted(news_items, key=lambda x: x.score)
-
-    # Example usage: printing the collected news items
-    for item in news_items:
-        print(f"Rank: {item.rank}, Title: {item.title}, Score: {item.score}, Comments: {item.comments}")
+    # Print to file
+    if outputfile_name:
+        with open(outputfile_name, 'w') as file:
+            for item in news_items:
+                file.write(f"Rank: {item.rank}, Title: {item.title}, Score: {item.score}, Comments: {item.comments}\n")
+    # Print to stdout
+    else:
+        for item in news_items:
+            print(f"Rank: {item.rank}, Title: {item.title}, Score: {item.score}, Comments: {item.comments}")
